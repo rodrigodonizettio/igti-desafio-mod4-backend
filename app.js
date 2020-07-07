bb8b30e4d2bcad1132e8608a6c1d9e84bd7cd289
Rodrigo Donizetti de Oliveira <rodrigodonizettio@gmail.com>
2020-07-07 16:52:58 -0300

    Changing CORS origin from localhost to Heroku

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: 'http://localhost:8080',
+    origin: 'https://damp-hamlet-27279.herokuapp.com/',
   })
 );
 
