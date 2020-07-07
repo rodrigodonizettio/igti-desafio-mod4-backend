14e8c4eebb6909300b11916de12d32f9c783a0c0
Rodrigo Donizetti de Oliveira <rodrigodonizettio@gmail.com>
2020-07-07 17:05:00 -0300

    Change CORS origin from Heroku to All

--- app.js
+++ app.js
@@ -31,9 +31,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
-  cors({
-    origin: 'https://damp-hamlet-27279.herokuapp.com/',
-  })
+  cors()
 );
 
 app.use(gradeRouter);
